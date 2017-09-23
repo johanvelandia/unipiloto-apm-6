d0330be70caa784b2a14300ca360707d624765aa
Miguel11mt <migttf_11@hotmail.com>
2017-09-23 09:19:46 -0500

    22/09
    22/09

---
+++ modulo_8/lab4/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { MyApp } from './app.component';
+import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
+import {ModalProfilePage} from "../pages/modal-profile/modal-profile";
+import { Page3 } from '../pages/page3/page3';
+import { Page4 } from '../pages/page4/page4';
+import { Page5 } from '../pages/page5/page5';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    Page1,
+    Page2,
+    ModalProfilePage,
+    Page3,
+    Page4,
+    Page5
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    Page1,
+    Page2,
+    ModalProfilePage,
+    Page3,
+    Page4,
+    Page5
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
